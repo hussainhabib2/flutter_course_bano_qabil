81ed2f794893dc97296426642cc37a2fa9cf0c9e
hussain <hussain@brandver.se>
2023-05-17 10:14:28 +0500

    folder renamed

---
+++ Lesson #4/README.md
@@ -0,0 +1,5 @@
+# Lesson #4
+
+Creating a login UI using Stack, Buttons, Column and CircleAvatar
+
+![Logo](lesson4.png)
---
+++ Lesson #4/android/app/src/main/kotlin/com/example/lesson_4/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lesson_4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
