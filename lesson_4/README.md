5750cdcc9c36d6fed3202c4d80045f9724fcb16e
hussain <hussain@brandver.se>
2023-05-17 10:15:26 +0500

    lesson 4 updated

--- lesson_4/README.md
+++
@@ -1,5 +0,0 @@
-# Lesson #4
-
-Creating a login UI using Stack, Buttons, Column and CircleAvatar
-
-![Logo](lesson4.png)
--- lesson_4/android/app/src/main/kotlin/com/example/lesson_4/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.lesson_4
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
