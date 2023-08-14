2f7fa1f253805b614e005a46c840b0789b78f6ce
hussain <hussain@brandver.se>
2023-08-14 13:29:49 +0500

    lesson 17 output

--- lesson_17/lib/views/first_screen.dart
+++ lesson_17/lib/views/first_screen.dart
@@ -15,63 +15,68 @@ class FirstScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Firebase Example'),
       ),
-      body: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              FirebaseFirestore firestore = FirebaseFirestore.instance;
-              CollectionReference usersCollection =
-                  firestore.collection('users');
-              usersCollection
-                  .add({
-                    'full_name': "Ubaid", // John Doe
-                    'company': "Bano Qabil", // Stokes and Sons
-                    'age': 23,
-                    'photos': [
-                      'myphoto.jpg',
-                      'myphoto2.jpg',
-                      'myphoto3.jpg',
-                    ]
-                  })
-                  .then((value) => print("User Added"))
-                  .catchError((error) => print("Failed to add user: $error"));
-            },
-            child: Text('Signup'),
-          ),
-          ElevatedButton(
-            onPressed: () async {
-              FirebaseFirestore firestore = FirebaseFirestore.instance;
-              CollectionReference usersCollection =
-                  firestore.collection('users');
-              QuerySnapshot querySnapshot = await usersCollection.get();
-              final allData =
-                  querySnapshot.docs.map((doc) => doc.data()).toList();
-              print(allData);
-            },
-            child: const Text('Get all users'),
-          ),
-          ElevatedButton(
-            onPressed: () async {
-              FilePickerResult? result = await FilePicker.platform.pickFiles();
-              Random random = Random();
-              int randomNumber = random.nextInt(100000000);
-              if (result != null) {
-                File file = File(result.files.single.path!);
-                final FirebaseStorage _storage = FirebaseStorage.instanceFor(
-                    bucket: 'gs://lesson17-b8092.appspot.com');
-                final Reference _imagesRef = _storage.ref().child(randomNumber.toString());
-                _imagesRef.putFile(file);
-              } else {
-                // User canceled the picker
-              }
-            },
-            child: const Text('Upload a file'),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Get a file'),
-          )
-        ],
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                FirebaseFirestore firestore = FirebaseFirestore.instance;
+                CollectionReference usersCollection =
+                    firestore.collection('users');
+                usersCollection
+                    .add({
+                      'full_name': "Ubaid", // John Doe
+                      'company': "Bano Qabil", // Stokes and Sons
+                      'age': 23,
+                      'photos': [
+                        'myphoto.jpg',
+                        'myphoto2.jpg',
+                        'myphoto3.jpg',
+                      ]
+                    })
+                    .then((value) => print("User Added"))
+                    .catchError((error) => print("Failed to add user: $error"));
+              },
+              child: Text('Signup'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                FirebaseFirestore firestore = FirebaseFirestore.instance;
+                CollectionReference usersCollection =
+                    firestore.collection('users');
+                QuerySnapshot querySnapshot = await usersCollection.get();
+                final allData =
+                    querySnapshot.docs.map((doc) => doc.data()).toList();
+                print(allData);
+              },
+              child: const Text('Get all users'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                FilePickerResult? result = await FilePicker.platform.pickFiles();
+                Random random = Random();
+                int randomNumber = random.nextInt(100000000);
+                if (result != null) {
+                  File file = File(result.files.single.path!);
+                  final FirebaseStorage _storage = FirebaseStorage.instanceFor(
+                      bucket: 'gs://lesson17-b8092.appspot.com');
+                  final Reference _imagesRef = _storage.ref().child(randomNumber.toString());
+                  _imagesRef.putFile(file);
+                } else {
+                  // User canceled the picker
+                }
+              },
+              child: const Text('Upload a file'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Get a file'),
+            )
+          ],
+        ),
       ),
     );
   }
