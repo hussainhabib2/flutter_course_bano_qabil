f98a7f4172fc4eee439d69920f332807a57771a9
hussain <hussain@brandver.se>
2023-08-14 11:57:06 +0500

    lesson 13 output

--- lesson_10/README.md
+++ lesson_10/README.md
@@ -1,5 +1,6 @@
 # Lesson #10
 
 Building Quotes App UI with the objective of learning SafeArea, RichText, Stack, and integrating packages
+Then also connecting with backend RESTFUL API using Dio
 
 ![Logo](output.png)
--- lesson_13/lib/first_screen.dart
+++ lesson_13/lib/first_screen.dart
@@ -4,97 +4,121 @@ import 'package:torch_light/torch_light.dart';
 class FirstScreen extends StatelessWidget {
   const FirstScreen({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Lesson 13"),
       ),
-      body: Column(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(40),
-            child: Container(
-              height: 250,
-              width: 250,
-              color: Colors.red,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text(
+              'Learning ClipRRect',
+              style: Theme.of(context).textTheme.headlineSmall,
             ),
-          ),
-          RichText(
-            text: TextSpan(
-                text: 'I am Hussain',
-                style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 25,
-                ),
-                children: [
-                  TextSpan(
-                    text: ' how are sad asd sad asd asd as dayou doing?',
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontSize: 25,
-                    ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(40),
+              child: Container(
+                height: 100,
+                width: 100,
+                color: Colors.red,
+              ),
+            ),
+            Text(
+              'Learning RichText',
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            RichText(
+              text: const TextSpan(
+                  text: 'I am Hussain',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 18,
                   ),
-                  TextSpan(
-                    text: ' how are sad asd sad asd asd as dayou doing?',
-                    style: TextStyle(
-                      color: Colors.greenAccent,
-                      fontSize: 25,
+                  children: [
+                    TextSpan(
+                      text: ' how are you doing?',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 18,
+                      ),
                     ),
-                  ),
-                ]),
-          ),
-          ElevatedButton(
-            onPressed: () async {
-              try {
-                await TorchLight.enableTorch();
-              } on Exception catch (_) {
-                // Handle error
-              }
-            },
-            child: Text('Open Torch'),
-          ),
-          MyStateful(Colors.red, key: Key('123'),),
-        ],
+                    TextSpan(
+                      text: ' Damn, this looks good!',
+                      style: TextStyle(
+                        color: Colors.green,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ]),
+            ),
+            Text(
+              'Adding Flashlight/Torch Package',
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                try {
+                  await TorchLight.enableTorch();
+                } on Exception catch (_) {
+                  // Handle error
+                }
+              },
+              child: Text('Open Torch'),
+            ),
+            Text(
+              'Learning Widget Lifecycle',
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            MyStateful(
+              Colors.red,
+              key: Key('123'),
+            ),
+          ],
+        ),
       ),
     );
   }
-
 
   dispose() {
     TorchLight.disableTorch();
   }
 }
 
-
 class MyStateful extends StatefulWidget {
-  const MyStateful(this.color,{Key? key}) : super(key: key);
+  const MyStateful(this.color, {Key? key}) : super(key: key);
   final Color color;
+
   @override
   State<MyStateful> createState() => _MyStatefulState();
 }
 
 class _MyStatefulState extends State<MyStateful> {
-
   @override
   void initState() {
-   print('hello initstate chal gya');
+    print('hello initstate executed');
     super.initState();
   }
 
   @override
   void didChangeDependencies() {
-   print('did change dependencies chal gya');
+    print('did change dependencies executed');
     super.didChangeDependencies();
   }
 
   TextEditingController controller = TextEditingController();
 
-
   @override
   Widget build(BuildContext context) {
-    return Container(color: widget.color,height: 100,width: 100,);
+    return Container(
+      color: widget.color,
+      height: 100,
+      width: 100,
+    );
   }
 
   @override
@@ -109,6 +133,4 @@ class _MyStatefulState extends State<MyStateful> {
     controller.dispose();
     super.dispose();
   }
-
 }
-
